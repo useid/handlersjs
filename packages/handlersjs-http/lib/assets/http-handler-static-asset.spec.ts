df44bff21764c04ce424ee4e0fc916838432b897
Arthur Joppart <arthur@digita.ai>
2021-04-21 16:40:50 +0200

    refactor: improved static asset handler and its tests significantly

--- packages/handlersjs-http/lib/assets/http-handler-static-asset.spec.ts
+++ packages/handlersjs-http/lib/assets/http-handler-static-asset.spec.ts
@@ -2,197 +2,68 @@ import { TestService, mainModulePath, configPath } from '../../setup-tests';
 import { HttpHandlerResponse } from '../general/http-handler-response';
 import { HttpHandlerContext } from '../general/http-handler-context';
 import { HttpHandlerStaticAssetService } from './http-handler-static-asset';
+import { NotFoundHttpError } from '../errors/not-found-http-error';
+import { ForbiddenHttpError } from '../errors/forbidden-http-error';
+import { UnsupportedMediaTypeHttpError } from '../errors/unsupported-media-type-http-error';
 
 describe('HttpHandlerStaticAssetService', () => {
   let service: HttpHandlerStaticAssetService;
-
+  
   beforeAll(async () => {
     service = await TestService.instantiate('urn:handlersjs-http:test:HttpHandlerStaticAssetService', mainModulePath, configPath);
+  });
+
+  let context: HttpHandlerContext;
+
+  beforeEach(() => {
+    context = {
+      request: {
+        url: new URL('http://example.com'),
+        method: 'GET',
+        headers: {
+          accept: 'text/plain',
+        },
+        parameters: {
+          filename: 'filler.file',
+        }
+      },
+    };
   });
 
   it('should be correctly instantiated', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('canHandle', () => {
-    it('throws a 415-error when content-type is not supported', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: { 
-            accept: 'application/json'
-          },
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-
-        service.canHandle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-          },
-          (err) => {
-            expect(err.constructor.name).toBe('UnsupportedMediaTypeHttpError');
-            expect(err.statusCode).toBe(415);
-            done();
-          },
-        );
-    });
-
-    it('should return true when correct accept header is specified', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: {             
-            accept: 'text/plain'
-          },
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-  
-        service.canHandle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-            expect(payload).toBeTruthy();
-            done();
-          },
-        );
-    });
-
-    it('should return true when no accept header is specified', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: { },
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-  
-        service.canHandle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-            expect(payload).toBeTruthy();
-            done();
-          },
-        );
+  describe('canHandle()', () => {
+    it('should always return true', async() => {
+      const response = service.canHandle(context).toPromise();
+      await expect(response).resolves.toBe(true);
     });
   });
 
-  describe('handle', () => {
-    it('throws a 403-error when filename is invalid', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: { },
-          parameters: {
-            filename: '../../test'
-          }
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-  
-        service.handle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-          },
-          (err) => {
-            expect(err.constructor.name).toBe('ForbiddenHttpError');
-            expect(err.statusCode).toBe(403);
-            done();
-          },
-        );
+  describe('handle()', () => {
+    it('throws an UnsupportedMediaTypeHttpError when content-type is not supported', async() => {
+      context.request.headers.accept = 'application/json';
+      const response = service.handle(context).toPromise();
+      await expect(response).rejects.toThrowError(new UnsupportedMediaTypeHttpError('Content type not supported'));
     });
 
-    it('throws a 404-error when file is not found', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: { },
-          parameters: {
-            filename: 'test.php'
-          }
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-  
-        service.handle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-          },
-          (err) => {
-            expect(err.constructor.name).toBe('NotFoundHttpError');
-            expect(err.statusCode).toBe(404);
-            done();
-          },
-        );
+    it('throws a ForbiddenHttpError when filename is invalid', async() => {
+      context.request.parameters.filename = '../../test.txt';
+      const response = service.handle(context).toPromise();
+      await expect(response).rejects.toThrowError(new ForbiddenHttpError(''));
     });
 
-    it('should return file content when file is found', async (done) => {
-      const context: HttpHandlerContext = {
-        route: null,
-        request: {
-          url: new URL('http://example.com'),
-          method: 'get',
-          headers: { },
-          parameters: {
-            filename: 'test.txt'
-          }
-        },
-      };
-  
-      const response: HttpHandlerResponse = {
-        body: null,
-        headers: {},
-        status: 200,
-      };
-  
-        service.handle(context)
-        .subscribe(
-          (payload) => {
-            // do nothing
-            expect(payload.body).toBe('test file');
-            done();
-          },
-        );
+    it('throws a NotFoundHttpError when file is not found', async() => {
+      context.request.parameters.filename = 'test.php';
+      const response = service.handle(context).toPromise();
+      await expect(response).rejects.toThrowError(new NotFoundHttpError('Error while trying to read file'));
+    });
+
+    it('should return file content when file is found', async() => {
+      context.request.parameters.filename = 'test.txt';
+      const response = await service.handle(context).toPromise();
+      expect(response.body).toBe('test file');
     });
   });
 });
