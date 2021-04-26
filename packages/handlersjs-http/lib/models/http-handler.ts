d807d8f89f8f5c64aae3e504b791d7ece359115e
Wouter Termont <woutermont@gmail.com>
2021-04-26 13:56:04 +0200

    feat: add preresponsehandler typings
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/handlersjs-http/lib/models/http-handler.ts
+++ packages/handlersjs-http/lib/models/http-handler.ts
@@ -3,7 +3,8 @@ import { Handler } from '@digita-ai/handlersjs-core';
 import { HttpHandlerResponse } from './http-handler-response';
 import { HttpHandlerContext } from './http-handler-context';
 
-export abstract class HttpHandler extends Handler<HttpHandlerContext, HttpHandlerResponse> {
-  public abstract canHandle(context: HttpHandlerContext, response?: HttpHandlerResponse): Observable<boolean>;
-  public abstract handle(context: HttpHandlerContext, response?: HttpHandlerResponse): Observable<HttpHandlerResponse>;
+export abstract class HttpHandler<C extends HttpHandlerContext = HttpHandlerContext>
+  extends Handler<C, HttpHandlerResponse> {
+  public abstract canHandle(context: C, response?: HttpHandlerResponse): Observable<boolean>;
+  public abstract handle(context: C, response?: HttpHandlerResponse): Observable<HttpHandlerResponse>;
 }
