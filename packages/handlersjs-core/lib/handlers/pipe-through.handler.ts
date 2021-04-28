9a300ae7af497745a99a6c424f937a3edba45b84
Arthur Joppart <arthur@digita.ai>
2021-04-28 15:47:53 +0200

    feat: pipethroughhandler

---
+++ packages/handlersjs-core/lib/handlers/pipe-through.handler.ts
@@ -0,0 +1,29 @@
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { HandlerArgumentError } from '../errors/handler-argument-error';
+import { Handler } from './handler';
+
+export class PipeThroughHandler<T, S> extends Handler<T, S> {
+  constructor(
+    public handlers: Handler<any, any>[],
+  ) {
+    super();
+    if (!this.handlers) {
+      throw new HandlerArgumentError('Argument this.handlers should be set.', this.handlers);
+    }
+  }
+
+  canHandle(input: any): Observable<boolean> {
+    return of(true);
+  }
+
+  handle(input: any): Observable<any> {
+    let tempInp = of(input);
+    for (const handler of this.handlers) {
+      tempInp = tempInp.pipe(
+        switchMap((inp) => handler.handle(inp)),
+      );
+    }
+    return tempInp;
+  }
+}
---
+++ packages/handlersjs-core/lib/handlers/typed-pipe-through.handler.ts
@@ -0,0 +1,62 @@
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { HandlerArgumentError } from '../errors/handler-argument-error';
+import { Handler } from './handler';
+
+type HandlerSequence1<A, B> = [ Handler<A, B> ];
+type HandlerSequence2<A, B, C> = [ Handler<A, B>, Handler<B, C> ];
+type HandlerSequence3<A, B, C, D> = [ Handler<A, B>, Handler<B, C>, Handler<C, D> ];
+type HandlerSequence4<A, B, C, D, E> = [ Handler<A, B>, Handler<B, C>, Handler<C, D>, Handler<D, E> ];
+
+export class TypedPipeThroughHandler<A, B, C, D, E> extends Handler<A, E> {
+  constructor(
+    public handlers: HandlerSequence1<A, E> |
+    HandlerSequence2<A, B, E> |
+    HandlerSequence3<A, B, C, E> |
+    HandlerSequence4<A, B, C, D, E>,
+  ) {
+    super();
+    if (!this.handlers) {
+      throw new HandlerArgumentError('Argument this.handlers should be set.', this.handlers);
+    }
+  }
+
+  canHandle(input: A): Observable<boolean> {
+    return of(true);
+  }
+
+  handle(input: A): Observable<E> {
+
+    switch (this.handlers.length) {
+    case 1: {
+      const AtoE = this.handlers[0];
+      return AtoE.handle(input);
+    }
+    case 2: {
+      const BtoE = this.handlers[1];
+      return this.handlers[0].handle(input).pipe(
+        switchMap((inp) => BtoE.handle(inp)),
+      );
+    }
+    case 3: {
+      const BtoC = this.handlers[1];
+      const CtoE = this.handlers[2];
+      return this.handlers[0].handle(input).pipe(
+        switchMap((inp) => BtoC.handle(inp)),
+        switchMap((inp) => CtoE.handle(inp)),
+      );
+    }
+    case 4: {
+      const BtoC = this.handlers[1];
+      const CtoD = this.handlers[2];
+      const DtoE = this.handlers[3];
+      return this.handlers[0].handle(input).pipe(
+        switchMap((inp) => BtoC.handle(inp)),
+        switchMap((inp) => CtoD.handle(inp)),
+        switchMap((inp) => DtoE.handle(inp)),
+      );
+    }
+    default: return of(undefined);
+    }
+  }
+}
