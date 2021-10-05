9b490e890966c4073eb17ae43a5852d88e14c11f
Jasper Vandenberghen <43341653+JasperVandenberghen@users.noreply.github.com>
2021-10-05 16:07:08 +0200

    fix: add-error-header-object (#115)
    * fix: added headers object to error response
    
    * chore: added conflict error response
    
    * chore: added all error code messages
    
    * test: added test for error responses
    
    * test: added default case test
    
    * chore: added code review suggestions

--- packages/handlersjs-http/lib/handlers/error.handler.spec.ts
+++ packages/handlersjs-http/lib/handlers/error.handler.spec.ts
@@ -56,16 +56,84 @@ describe('error_handler', () => {
     ${errorHandlerFalse}| ${response}                           | ${`Bad Request`}
     ${errorHandlerTrue} | ${{ ...response, status: 401 }}       | ${`Unauthorized: ${response.body}`}
     ${errorHandlerFalse}| ${{ ...response, status: 401 }}       | ${`Unauthorized`}
+    ${errorHandlerTrue} | ${{ ...response, status: 402 }}       | ${`Payment Required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 402 }}       | ${`Payment Required`}
     ${errorHandlerTrue} | ${{ ...response, status: 403 }}       | ${`Forbidden: ${response.body}`}
     ${errorHandlerFalse}| ${{ ...response, status: 403 }}       | ${`Forbidden`}
     ${errorHandlerTrue} | ${{ ...response, status: 404 }}       | ${`Not Found: ${response.body}`}
     ${errorHandlerFalse}| ${{ ...response, status: 404 }}       | ${`Not Found`}
     ${errorHandlerTrue} | ${{ ...response, status: 405 }}       | ${`Method Not Allowed: ${response.body}`}
     ${errorHandlerFalse}| ${{ ...response, status: 405 }}       | ${`Method Not Allowed`}
+    ${errorHandlerTrue} | ${{ ...response, status: 406 }}       | ${`Not Acceptable: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 406 }}       | ${`Not Acceptable`}
+    ${errorHandlerTrue} | ${{ ...response, status: 407 }}       | ${`Proxy Authentication Required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 407 }}       | ${`Proxy Authentication Required`}
+    ${errorHandlerTrue} | ${{ ...response, status: 408 }}       | ${`Request Timeout: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 408 }}       | ${`Request Timeout`}
+    ${errorHandlerTrue} | ${{ ...response, status: 409 }}       | ${`Conflict: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 409 }}       | ${`Conflict`}
+    ${errorHandlerTrue} | ${{ ...response, status: 410 }}       | ${`Gone: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 410 }}       | ${`Gone`}
+    ${errorHandlerTrue} | ${{ ...response, status: 411 }}       | ${`Length Required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 411 }}       | ${`Length Required`}
+    ${errorHandlerTrue} | ${{ ...response, status: 412 }}       | ${`Precondition Failed: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 412 }}       | ${`Precondition Failed`}
+    ${errorHandlerTrue} | ${{ ...response, status: 413 }}       | ${`Payload Too Large: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 413 }}       | ${`Payload Too Large`}
+    ${errorHandlerTrue} | ${{ ...response, status: 414 }}       | ${`URI Too Long: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 414 }}       | ${`URI Too Long`}
+    ${errorHandlerTrue} | ${{ ...response, status: 415 }}       | ${`Unsupported Media Type: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 415 }}       | ${`Unsupported Media Type`}
+    ${errorHandlerTrue} | ${{ ...response, status: 416 }}       | ${`Range Not Satisfiable: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 416 }}       | ${`Range Not Satisfiable`}
+    ${errorHandlerTrue} | ${{ ...response, status: 417 }}       | ${`Expectation Failed: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 417 }}       | ${`Expectation Failed`}
+    ${errorHandlerTrue} | ${{ ...response, status: 418 }}       | ${`I'm a teapot: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 418 }}       | ${`I'm a teapot`}
+    ${errorHandlerTrue} | ${{ ...response, status: 421 }}       | ${`Misdirected Request: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 421 }}       | ${`Misdirected Request`}
+    ${errorHandlerTrue} | ${{ ...response, status: 422 }}       | ${`Unprocessable Entity: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 422 }}       | ${`Unprocessable Entity`}
+    ${errorHandlerTrue} | ${{ ...response, status: 423 }}       | ${`Locked: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 423 }}       | ${`Locked`}
+    ${errorHandlerTrue} | ${{ ...response, status: 424 }}       | ${`Failed Dependency: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 424 }}       | ${`Failed Dependency`}
+    ${errorHandlerTrue} | ${{ ...response, status: 425 }}       | ${`Too Early: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 425 }}       | ${`Too Early`}
+    ${errorHandlerTrue} | ${{ ...response, status: 426 }}       | ${`Upgrade Required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 426 }}       | ${`Upgrade Required`}
+    ${errorHandlerTrue} | ${{ ...response, status: 428 }}       | ${`Precondition required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 428 }}       | ${`Precondition required`}
+    ${errorHandlerTrue} | ${{ ...response, status: 429 }}       | ${`Too Many Requests: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 429 }}       | ${`Too Many Requests`}
+    ${errorHandlerTrue} | ${{ ...response, status: 431 }}       | ${`Request Header Fields Too Large: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 431 }}       | ${`Request Header Fields Too Large`}
+    ${errorHandlerTrue} | ${{ ...response, status: 451 }}       | ${`Unavailable For Legal Reasons: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 451 }}       | ${`Unavailable For Legal Reasons`}
     ${errorHandlerTrue} | ${{ ...response, status: 500 }}       | ${`Internal Server Error: ${response.body}`}
     ${errorHandlerFalse}| ${{ ...response, status: 500 }}       | ${`Internal Server Error`}
-    ${errorHandlerTrue} | ${{ ...response, status: 409 }}       | ${`An Unexpected Error Occurred: ${response.body}`}
-    ${errorHandlerFalse}| ${{ ...response, status: 409 }}       | ${`An Unexpected Error Occurred`}
+    ${errorHandlerTrue} | ${{ ...response, status: 501 }}       | ${`Not Implemented: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 501 }}       | ${`Not Implemented`}
+    ${errorHandlerTrue} | ${{ ...response, status: 502 }}       | ${`Bad Gateway: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 502 }}       | ${`Bad Gateway`}
+    ${errorHandlerTrue} | ${{ ...response, status: 503 }}       | ${`Service Unavailable: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 503 }}       | ${`Service Unavailable`}
+    ${errorHandlerTrue} | ${{ ...response, status: 504 }}       | ${`Gateway Timeout: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 504 }}       | ${`Gateway Timeout`}
+    ${errorHandlerTrue} | ${{ ...response, status: 505 }}       | ${`HTTP Version Not Supported: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 505 }}       | ${`HTTP Version Not Supported`}
+    ${errorHandlerTrue} | ${{ ...response, status: 506 }}       | ${`Variant Also Negotiates: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 506 }}       | ${`Variant Also Negotiates`}
+    ${errorHandlerTrue} | ${{ ...response, status: 507 }}       | ${`Insufficient Storage: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 507 }}       | ${`Insufficient Storage`}
+    ${errorHandlerTrue} | ${{ ...response, status: 508 }}       | ${`Loop detected: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 508 }}       | ${`Loop detected`}
+    ${errorHandlerTrue} | ${{ ...response, status: 510 }}       | ${`Not Extended: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 510 }}       | ${`Not Extended`}
+    ${errorHandlerTrue} | ${{ ...response, status: 511 }}       | ${`Network Authentication Required: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 511 }}       | ${`Network Authentication Required`}
+    ${errorHandlerTrue} | ${{ ...response, status: 555 }}       | ${`An Unexpected Error Occurred: ${response.body}`}
+    ${errorHandlerFalse}| ${{ ...response, status: 555 }}       | ${`An Unexpected Error Occurred`}
   `('should return $expected when handler is $a and response is $b', async ({ handler, resp, expected }) => {
 
       const res = await handler.handle(resp).toPromise();
--- packages/handlersjs-http/lib/handlers/error.handler.ts
+++ packages/handlersjs-http/lib/handlers/error.handler.ts
@@ -29,10 +29,44 @@ export class ErrorHandler extends Handler<HttpHandlerResponse, HttpHandlerRespon
 
       case 400: return of(this.createErrorResponse(response, 'Bad Request', this.showUpstreamError));
       case 401: return of(this.createErrorResponse(response, 'Unauthorized', this.showUpstreamError));
+      case 402: return of(this.createErrorResponse(response, 'Payment Required', this.showUpstreamError));
       case 403: return of(this.createErrorResponse(response, 'Forbidden', this.showUpstreamError));
       case 404: return of(this.createErrorResponse(response, 'Not Found', this.showUpstreamError));
       case 405: return of(this.createErrorResponse(response, 'Method Not Allowed', this.showUpstreamError));
+      case 406: return of(this.createErrorResponse(response, 'Not Acceptable', this.showUpstreamError));
+      case 407: return of(this.createErrorResponse(response, 'Proxy Authentication Required', this.showUpstreamError));
+      case 408: return of(this.createErrorResponse(response, 'Request Timeout', this.showUpstreamError));
+      case 409: return of(this.createErrorResponse(response, 'Conflict', this.showUpstreamError));
+      case 410: return of(this.createErrorResponse(response, 'Gone', this.showUpstreamError));
+      case 411: return of(this.createErrorResponse(response, 'Length Required', this.showUpstreamError));
+      case 412: return of(this.createErrorResponse(response, 'Precondition Failed', this.showUpstreamError));
+      case 413: return of(this.createErrorResponse(response, 'Payload Too Large', this.showUpstreamError));
+      case 414: return of(this.createErrorResponse(response, 'URI Too Long', this.showUpstreamError));
+      case 415: return of(this.createErrorResponse(response, 'Unsupported Media Type', this.showUpstreamError));
+      case 416: return of(this.createErrorResponse(response, 'Range Not Satisfiable', this.showUpstreamError));
+      case 417: return of(this.createErrorResponse(response, 'Expectation Failed', this.showUpstreamError));
+      case 418: return of(this.createErrorResponse(response, `I'm a teapot`, this.showUpstreamError));
+      case 421: return of(this.createErrorResponse(response, 'Misdirected Request', this.showUpstreamError));
+      case 422: return of(this.createErrorResponse(response, 'Unprocessable Entity', this.showUpstreamError));
+      case 423: return of(this.createErrorResponse(response, 'Locked', this.showUpstreamError));
+      case 424: return of(this.createErrorResponse(response, 'Failed Dependency', this.showUpstreamError));
+      case 425: return of(this.createErrorResponse(response, 'Too Early', this.showUpstreamError));
+      case 426: return of(this.createErrorResponse(response, 'Upgrade Required', this.showUpstreamError));
+      case 428: return of(this.createErrorResponse(response, 'Precondition required', this.showUpstreamError));
+      case 429: return of(this.createErrorResponse(response, 'Too Many Requests', this.showUpstreamError));
+      case 431: return of(this.createErrorResponse(response, 'Request Header Fields Too Large', this.showUpstreamError));
+      case 451: return of(this.createErrorResponse(response, 'Unavailable For Legal Reasons', this.showUpstreamError));
       case 500: return of(this.createErrorResponse(response, 'Internal Server Error', this.showUpstreamError));
+      case 501: return of(this.createErrorResponse(response, 'Not Implemented', this.showUpstreamError));
+      case 502: return of(this.createErrorResponse(response, 'Bad Gateway', this.showUpstreamError));
+      case 503: return of(this.createErrorResponse(response, 'Service Unavailable', this.showUpstreamError));
+      case 504: return of(this.createErrorResponse(response, 'Gateway Timeout', this.showUpstreamError));
+      case 505: return of(this.createErrorResponse(response, 'HTTP Version Not Supported', this.showUpstreamError));
+      case 506: return of(this.createErrorResponse(response, 'Variant Also Negotiates', this.showUpstreamError));
+      case 507: return of(this.createErrorResponse(response, 'Insufficient Storage', this.showUpstreamError));
+      case 508: return of(this.createErrorResponse(response, 'Loop detected', this.showUpstreamError));
+      case 510: return of(this.createErrorResponse(response, 'Not Extended', this.showUpstreamError));
+      case 511: return of(this.createErrorResponse(response, 'Network Authentication Required', this.showUpstreamError));
       default: return response.status < 600 && response.status >= 400
         ? of(this.createErrorResponse(response, 'An Unexpected Error Occurred', this.showUpstreamError))
         : of(response);
