a59790e5b69caa73c4bc72ba1ef9e681ac35abf3
Arthur Joppart <arthur@digita.ai>
2023-04-20 15:38:29 +0200

    feat: move logger to a global variable

--- packages/handlersjs-logging/lib/main.ts
+++ packages/handlersjs-logging/lib/main.ts
@@ -1,52 +1,32 @@
-import { LoggerOptions } from './models/logger-options';
-import { Logger } from './models/logger';
-import { LoggerFactory } from './factories/logger-factory';
+import { Logger } from './loggers/logger';
 
-let logger: Logger;
-let loggerFactory: LoggerFactory;
+const loggerKey = '__LOGGER__HANDLERSJS__LOGGING__';
 
-/**
- * returns the global logger.
- *
- * @returns { Logger }
- */
-export const getLogger = (): Logger => logger;
+export const setLogger = (
+  logger: Logger,
+): void => {
 
-/**
- * Sets the global logger.
- * This will cause the logger returned by {@link getLogger} to be changed.
- *
- * @param { Logger } logger - The (new) logger to set globally.
- */
-export const setLogger = (newLogger: Logger): void => {
-
-  logger = newLogger;
+  setGlobalLogger(logger);
 
 };
 
-/**
- * Gets a logger instance for the given class instance.
- *
- * @param loggable - A class instance or a class string name.
- */
-export const getLoggerFor = (
-  loggable: string | { constructor: { name: string } },
-  loggerOptions?: LoggerOptions
-): Logger => {
+export const getLogger = (): Logger => {
 
-  if (!loggerFactory) {
-
-    throw new Error('No LoggerFactory was set to create loggers.');
-
-  }
-
-  return loggerFactory.createLogger(typeof loggable === 'string' ? loggable : loggable.constructor.name, loggerOptions);
+  const logger = getGlobalLogger();
+  if (!logger) throw new Error('No logger was set. Set a logged using setLogger()');
+  return logger;
 
 };
 
-export const setLoggerFactory = (newLoggerFactory: LoggerFactory): void => {
 
-  loggerFactory = newLoggerFactory;
+const getGlobalLogger = (): Logger => {
+  
+  return (global as any)[loggerKey];
+  
+}
 
-};
+const setGlobalLogger = (newLogger: Logger): void => {
 
+  (global as any)[loggerKey] = newLogger;
+
+}
