bfae6df755df3198a37041dd4601928b8e9eb0fe
Arthur Joppart <38424924+BelgianNoise@users.noreply.github.com>
2023-05-15 10:01:06 +0200

    fix: don't crash server when a non HTTP method was provided (#236)

--- packages/handlersjs-http/lib/servers/node/node-http-request-response.handler.spec.ts
+++ packages/handlersjs-http/lib/servers/node/node-http-request-response.handler.spec.ts
@@ -120,6 +120,19 @@ describe('NodeHttpRequestResponseHandler', () => {
 
     });
 
+    it('should return 405 when request method is not an HTTP method', async () => {
+
+      // This is a valid header in the WebDAV protocol
+      streamMock.requestStream.method = 'PROPFIND';
+      await lastValueFrom(handler.handle(streamMock));
+
+      expect(streamMock.responseStream.writeHead).toHaveBeenCalledWith(
+        405,
+        'Only HTTP methods are allowed!',
+      );
+
+    });
+
     it('throws when headers is null/undefined', async () => {
 
       streamMock.requestStream.headers = null;
